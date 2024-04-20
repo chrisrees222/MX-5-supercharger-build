2717e4f47e5c4ba864cbef2f62cd29f88cef304b
chrisrees222 <143870938+chrisrees222@users.noreply.github.com>
2024-04-20 23:42:45 +0100

    comments js

---
+++ MX-5 supercharger build/Pages/Index1.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MX_5_supercharger_build.Pages
+{
+    public class Index1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- MX-5 supercharger build/wwwroot/js/site.js
+++ MX-5 supercharger build/wwwroot/js/site.js
@@ -2,3 +2,34 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function addRecommendation() {
+    // Get the message of the new recommendation
+    let recommendation = document.getElementById("new_recommendation");
+    let siteuser = document.getElementById("user");
+
+    // If the user has left a recommendation, display a pop-up
+    if (recommendation.value && siteuser.value != null && recommendation.value.trim() != "") {
+        console.log("New comment added");
+        //Call showPopup here
+        showPopup(true);
+        // Create a new 'recommendation' element and set it's value to the user's message
+        var element = document.createElement("div");
+        element.setAttribute("class", "recommendation");
+        element.innerHTML = "\<span\>&#8220;\</span\>" + siteuser.value + "<br>" + recommendation.value + "\<span\>&#8221;\</span\>";
+        // Add this element to the end of the list of recommendations
+        document.getElementById("all_recommendations").appendChild(element);
+
+        // Reset the value of the textarea
+        recommendation.value = "";
+        siteuser.value = "";
+    }
+}
+
+function showPopup(bool) {
+    if (bool) {
+        document.getElementById('popup').style.visibility = 'visible'
+    } else {
+        document.getElementById('popup').style.visibility = 'hidden'
+    }
+}
